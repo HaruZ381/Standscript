43bf3358e11cd52d4fadb60c938eb8ceb03e510a
HaruZ381 <harukisama212@gmail.com>
2025-09-02 12:38:37 +0700

    Add StandScript.lua

---
+++ standscript.lua
@@ -0,0 +1,28 @@
+-- Player Control Settings
+getgenv().TargetUsername = "TênTàiKhoảnChính"
+getgenv().standPosition = "right"
+getgenv().PerformanceMode = false
+
+-- Activation Commands
+getgenv().SummonWord = "Za Warudo!"
+getgenv().DismissWord = "Bye bye"
+
+-- Combat Commands
+getgenv().SkillWord1 = "1"
+getgenv().SkillWord2 = "2"
+getgenv().SkillWord3 = "3"
+getgenv().SkillWord4 = "4"
+
+-- Combat Voice Lines
+getgenv().Skill1Quote = "Muda da. Kore de saigo da!"
+getgenv().Skill2Quote = "GRAAH! Omae wa ore ni makenai!"
+getgenv().Skill3Quote = "Tch. Dōshi te oite!"
+getgenv().Skill4Quote = "Uppākatto! Subete o owari ni suru"
+getgenv().CooldownMessage = "Tch. Cooldown's not done yet. Be patient!"
+getgenv().RetreatQuote = "Consider yourself lucky... this isn’t over!"
+
+-- Summon/Dismiss Voice Lines
+getgenv().SummonQuote1 = "Hmph. Let's get this over with."
+getgenv().SummonQuote2 = "The World at your service."
+getgenv().DisappearQuote1 = "You've got it. I'm out of here"
+getgenv().DisappearQuote2 = "Fine. Don't mess this up"
